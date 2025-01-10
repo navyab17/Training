f96f87c1bdeb586ad1514a68948103766b2bce12
navyab17 <navyab1707@gmail.com>
2025-01-10 22:26:52 +0530

    Update day3.js

--- day3.js
+++ day3.js
@@ -1,16 +1,14 @@
-let a = prompt("Enter a number for A")
-let b = prompt("Enter a number for B")
-let c = prompt("Enter a number for C")
-
-if(a > b && a > c){
-    console.log("A is the largest number")
-}else if(b > a && b > c){
-    console.log("B is the largest number")
-}else if(c > a && c > b){
-    console.log("C is the largest number")
-}else if(a === b && a === c){
-    console.log("All numbers are equal")
-}
-else{
-    console.log("Enter a Valid Number")
+for (let i = 1; i <= 100; i++) {
+    if (i % 3 === 0 && i % 5 === 0) {
+        console.log('FizzBuzz');
+    } 
+    else if (i % 3 === 0) {
+        console.log('Fizz');
+    } 
+    else if (i % 5 === 0) {
+        console.log('Buzz');
+    } 
+    else {
+        console.log(i);
+    }
 }
