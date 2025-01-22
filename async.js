95a44fe571e008d8a5362716306343341a1408f0
navyab17 <navyab1707@gmail.com>
2025-01-22 10:31:24 +0530

    Update async.js

--- async.js
+++ async.js
@@ -27,3 +27,45 @@ let p3=new Promise((resolve,reject)=>{
     reject("Failures");
 });
 //console.log(p3);
+
+//function fetchUsers(){
+  //let res = window.fetch("https://api.github.com/users")
+  //console.log(res);
+  //res.then(val=>{
+    //return val.json().theen(data=>{
+      //console.log(data);
+    //})
+  //}).catch(err => {
+    //console.log(err);
+//})
+  //}
+ // fetchUsers();
+
+// ! Api fetching
+function fetchUsers(){
+  let res = window.fetch("https://jsonplaceholder.typicode.com/users")
+  //console.log(res);
+  res.then(val => {
+      //console.log(val);
+      return val.json().then(data => {
+          console.log(data);
+          let store = document.getElementById("store");
+          //console.log(store);
+          data.map((user) => {
+              //console.log(user);
+              store.innerHTML += `
+              <tr>
+                  <td>${user.id}</td>
+                  <td>${user.name}</td>
+                  <td>${user.email}</td>
+                  <td>${user.company.name}</td>
+                  <td>${user.phone}</td>
+              </tr>
+              `
+          })
+      });
+  }).catch(err => {
+      console.log(err);
+  })
+  }
+  fetchUsers();
