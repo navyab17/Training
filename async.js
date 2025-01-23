105fb6a07b10f24212182f866b2f3bb04045a627
navyab17 <navyab1707@gmail.com>
2025-01-23 10:35:57 +0530

    Update async.js

--- async.js
+++ async.js
@@ -69,3 +69,31 @@ function fetchUsers(){
   })
   }
   fetchUsers();
+
+//let p=new Promise((resolve,reject)=>{
+//    setTimeout(()=>{
+  //    resolve("Promise successfully completing");
+    //},3000)
+  //})
+//async function demo()
+  //{
+    //console.log("Start");
+    //let x=await p;
+    //console.log(x);
+    //console.log("End");
+  //}
+  //demo();
+
+//async function fetchUsers(){
+//  let response=await fetch("https://api.github.com/users");
+//  let data=await response.json();
+//  console.log(data);
+//}
+//fetchUsers();
+
+//async function fetchUsers(){
+//  let response=await fetch("https://jsonplaceholder.typicode.com/users");
+//  let data=await response.json();
+//  console.log(data);
+//}
+//fetchUsers();
